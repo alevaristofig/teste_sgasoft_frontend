025fceb3bbbbe69efaf57068cf8d5d0142b0e8c0
Alexandre <alevaristofig@gmail.com>
2025-05-13 23:06:58 -0300

    Criação da pagina de listagem e do mixin de produtos

--- src/components/produtos/ProdutosComponent.vue
+++ src/components/produtos/ProdutosComponent.vue
@@ -1,12 +1,51 @@
 <template>
     <div>
-        Produtos
+        <router-link class="list-group-item mt-3" :to="{ name: 'cadastrofornecedor'}">
+            <button class="btn btn-primary">Adicionar</button>
+        </router-link>  
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">FORNECEDOR</th>
+                    <th scope="col">NOME</th>
+                    <th scope="col">REFERÊNCIA</th>
+                    <th scope="col">COR</th>
+                    <th scope="col">PREÇO</th>                    
+                    <th scope=""></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(produto,i) in produtos" :key="{i}">
+                    <td>{{produto.id}}</td>
+                    <td>{{produto.fornecedor_id}}</td> 
+                    <td>{{produto.nome}}</td>
+                    <td>{{produto.referencia}}</td>
+                    <td>{{produto.cor}}</td>
+                    <td>{{produto.preco}}</td>                                       
+                    <td>
+                        <router-link :to="{name: 'editarfornecedor', params: {id: produto.id}}" class="btn btn-sm btn-primary me-2">
+                            <i class="bi bi-pencil-square" style="cursor: pointer"></i>
+                        </router-link>                         
+                        <button class="btn btn-sm btn-danger" @click="apagar(produto.id)">
+                            <i class="bi bi-trash-fill" style="cursor: pointer"></i>
+                        </button>
+                    </td>
+                    
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
+    import ProdutoMixins from '@/mixins/ProdutoMixins';
+
     export default {
-    name: 'ProdutosComponent',
-    
+        name: 'ProdutosComponent',
+        mixins: [ProdutoMixins],
+        created() {
+            this.listar();
+        }    
     }
 </script>
---
+++ src/mixins/ProdutoMixins.js
@@ -0,0 +1,145 @@
+import axios from "axios";
+
+export default {
+    data: () => ({
+        produtos: [],
+        nome: '',
+        cnpj: '',
+        cep: '',
+        endereco: '',
+        status: '',
+        classNome: false,
+        classCnpj: false,
+        classCep: false,
+        classStatus: false,
+        classEndereco: false,
+    }),
+    methods: {
+        listar(){                
+            axios.get('http://localhost:8000/api/v1/produtos')
+                    .then((response) => {                                                                    
+                        this.produtos = response.data.data;
+                        console.log(response.data.data); 
+                    })
+                    .catch((error) =>{                        
+                        console.log(error);
+            })
+        },
+      /*  buscar(id) {
+            axios.get(`http://localhost:8000/api/v1/fornecedores/${id}`)
+                .then((response) => {                                        
+                    this.nome = response.data.nome;
+                    this.cnpj = response.data.cnpj;
+                    this.cep = response.data.cep;
+                    this.status = response.data.status;
+                    this.endereco = response.data.endereco;
+                                    
+                })
+                .catch((error) =>{
+                    console.log(error);
+                    alert('Ocorreu um erro');                    
+            });           
+        },
+        salvar() {           
+            if(this.validarCampos()) {                  
+                let data = {
+                    'nome': this.nome,
+                    'cnpj': this.cnpj,
+                    'cep': this.cep,
+                    'status': this.status,
+                    'endereco': this.endereco
+                }
+
+                axios.post(`http://localhost:8000/api/v1/fornecedores`,data)
+                    .then(() => {
+                        alert('Usuario cadastrado com sucesso');   
+                        this.nome = '';
+                        this.cnpj = '';
+                        this.cep = '';
+                        this.status = '';
+                        this.endereco = '';                     
+                    })
+                    .catch((error) =>{
+                        alert('Ocorreu um erro');
+                        console.log(error);
+                });
+            }
+        },
+        apagar(id) {
+            axios.delete(`http://localhost:8000/api/v1/fornecedores/${id}`)
+                .then(() => {                                        
+                    alert('Fornecedor deletado com sucesso!');                                 
+                })
+                .catch((error) =>{
+                    console.log(error);
+                    alert('Ocorreu um erro');                    
+            });     
+        },
+        editar(id) {           
+            if(this.validarCampos()) {                                                
+
+                let data = {
+                    'nome': this.nome,
+                    'cnpj': this.cnpj,
+                    'cep': this.cep,
+                    'status': this.status,
+                    'endereco': this.endereco
+                }
+
+                axios.put(`http://localhost:8000/api/v1/fornecedores/${id}`,data)
+                    .then(() => {
+                        alert('Fornecedor atualizado com sucesso');
+                        this.$router.push({ name: 'fornecedor'});
+                    })
+                    .catch((error) =>{
+                        alert('Ocorreu um erro');
+                        console.log(error);
+                });
+            }           
+        },
+        validarCampos() {    
+            let erro = false;
+
+            if(this.nome === '') {                
+                this.classNome = true;
+                erro = true;
+            } else {
+                this.classNome = false;
+            }
+
+            if(this.cnpj === '') {                
+                this.classCnpj = true;
+                erro = true;
+            } else {
+                this.classCnpj = false;
+            }
+
+            if(this.cep === '') {                
+                this.classCep = true;
+                erro = true;
+            } else {
+                this.classCep = false;
+            }
+
+            if(this.status === '') {                            
+                this.classStatus = true;
+                erro = true;
+            } else {
+                this.classStatus = false;
+            }
+
+            if(this.endereco === '') {                
+                this.classEndereco = true;
+                erro = true;
+            } else {
+                this.classEndereco = false;
+            }
+
+            if(erro) {
+                return false;
+            }
+
+            return true;
+        }*/
+    }
+}
